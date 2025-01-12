ae11dbf23d3830995f4aedc7c884a717923fd9c7
Melansia <26363056+Melansia@users.noreply.github.com>
2025-01-12 19:47:52 +0200

    prepared the function for calculating the squares and drawing the board, for start it will be a 16x16

--- javascript.js
+++ javascript.js
@@ -1,1 +1,21 @@
-alert("Hello World!")
+const container = document.querySelector(".board-container");
+const boardSize = 16;
+const size = Math.floor(container.offsetWidth / boardSize);
+
+
+
+function drawBoard(size) {
+    for (let i = 0; i < boardSize * boardSize; i++) {
+        const square = document.createElement("div");
+        square.style.width = `${size}px`;
+        square.style.height = `${size}px`;
+        square.style.border = "0.1px solid lightgray";
+        square.style.boxSizing = "border-box";
+        square.style.flexGrow = "1"
+        square.style.flexShrink = "0";
+        container.appendChild(square);
+    }
+}
+
+drawBoard(size)
+
