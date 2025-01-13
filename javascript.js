f395888bd71bd838c37324725f905d9a5755b896
Melansia <26363056+Melansia@users.noreply.github.com>
2025-01-13 19:38:37 +0200

    added event listener to the board to handle the drawing functionality, added board resize functionality

--- javascript.js
+++ javascript.js
@@ -1,21 +1,61 @@
 const container = document.querySelector(".board-container");
-const boardSize = 16;
-const size = Math.floor(container.offsetWidth / boardSize);
+const resetButton = document.querySelector(".reset")
+const resizeButton = document.querySelector(".resize")
+const boardInfo = document.querySelector(".info")
+let isMouseDown = false
+let boardSize = 16;
 
+function drawBoard(bSize) {
 
+    while(container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
 
-function drawBoard(size) {
-    for (let i = 0; i < boardSize * boardSize; i++) {
+    const size = Math.floor(container.offsetWidth / bSize);
+
+    for (let i = 0; i < bSize * bSize; i++) {
         const square = document.createElement("div");
         square.style.width = `${size}px`;
         square.style.height = `${size}px`;
-        square.style.border = "0.1px solid lightgray";
+        square.style.border = "0.1px solid lightgrey";
         square.style.boxSizing = "border-box";
         square.style.flexGrow = "1"
         square.style.flexShrink = "0";
+        square.style.backgroundColor = "white";
+        square.classList.add("square")
         container.appendChild(square);
     }
+
+    boardInfo.textContent = `${bSize} x ${bSize}`
 }
 
-drawBoard(size)
+drawBoard(boardSize)
 
+resetButton.addEventListener("click", () => {
+    drawBoard(boardSize)
+})
+
+
+// TODO: implement a better error handling
+resizeButton.addEventListener("click", () => {
+    const newSize = parseInt(prompt("Enter the size of the new grid."));
+    boardSize = newSize
+    drawBoard(newSize)
+})
+
+container.addEventListener("mousedown", (event) => {
+    isMouseDown = true;
+    if (event.target.classList.contains("square")) {
+        event.target.style.backgroundColor = "orange";
+    }
+});
+
+container.addEventListener("mousemove", (event) => {
+    if (isMouseDown && event.target.classList.contains("square")) {
+        event.target.style.backgroundColor = "orange";
+    }
+});
+
+document.addEventListener("mouseup", () => {
+    isMouseDown = false;
+});
